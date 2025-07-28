a885dda0787ce9971c64c52537f00dde7f6c7b9d
ampfa <a.pfaehler225@gmail.com>
2025-07-28 09:23:32 -0400

    adding projects

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const projects = [
     description: "A JS-powered quiz to recommend TVs with affiliate links, based on room, budget, and usage.",
     link: "https://tvfinder.github.io/which-tv-should-i-buy/"
   }
+  {
+    name: "TV Finder Quiz",
+    description: "A focused, single-page landing site designed to introduce a product or platform—clear and uncluttered. Consists of a HTML, CSS, and little bit of JS to. First real world project for a business.",
+    link: "https://ampfa.github.io/Landingpage/"
+  }
 ];
 
 const projectList = document.getElementById("projectList");
